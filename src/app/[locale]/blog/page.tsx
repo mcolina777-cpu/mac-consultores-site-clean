46eeebdc54033c05df9597380469c400ba32dd98
mcolina777-cpu <mcolina777@gmail.com>
2026-06-24 09:17:48 -0400

    Fix blog dictionary keys

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -31,24 +31,24 @@ export default async function BlogIndex({ params }: Props) {
         <div className="container">
           <div className="grid-3">
             <article className="card">
-              <span className="section-tag">{dict?.blog?.article_1?.tag}</span>
-              <h3>{dict?.blog?.article_1?.title}</h3>
-              <p>{dict?.blog?.article_1?.desc}</p>
-              <Link href={`/${locale}/blog/criminalidad-economica`} className="card-link">{dict?.blog?.read_more}</Link>
+              <span className="section-tag">{dict?.blog?.article1?.tag}</span>
+              <h3>{dict?.blog?.article1?.title}</h3>
+              <p>{dict?.blog?.article1?.desc}</p>
+              <Link href={`/${locale}/blog/criminalidad-economica`} className="card-link">{dict?.blog?.readmore}</Link>
             </article>
             
             <article className="card">
-              <span className="section-tag">{dict?.blog?.article_2?.tag}</span>
-              <h3>{dict?.blog?.article_2?.title}</h3>
-              <p>{dict?.blog?.article_2?.desc}</p>
-              <Link href={`/${locale}/blog/amparo-garantia-vital`} className="card-link">{dict?.blog?.read_more}</Link>
+              <span className="section-tag">{dict?.blog?.article2?.tag}</span>
+              <h3>{dict?.blog?.article2?.title}</h3>
+              <p>{dict?.blog?.article2?.desc}</p>
+              <Link href={`/${locale}/blog/amparo-garantia-vital`} className="card-link">{dict?.blog?.readmore}</Link>
             </article>
             {/* Note: In backup, the 3rd card was El Régimen de Poderes en el CPC y el COPP */}
             <article className="card">
-              <span className="section-tag">{(dict?.blog as any)?.article_4?.tag}</span>
-              <h3>{(dict?.blog as any)?.article_4?.title}</h3>
-              <p>{(dict?.blog as any)?.article_4?.desc}</p>
-              <Link href={`/${locale}/blog/regimen-poderes-cpc-copp`} className="card-link">{dict?.blog?.read_more}</Link>
+              <span className="section-tag">{dict?.blog?.article4?.tag}</span>
+              <h3>{dict?.blog?.article4?.title}</h3>
+              <p>{dict?.blog?.article4?.desc}</p>
+              <Link href={`/${locale}/blog/regimen-poderes-cpc-copp`} className="card-link">{dict?.blog?.readmore}</Link>
             </article>
           </div>
         </div>
