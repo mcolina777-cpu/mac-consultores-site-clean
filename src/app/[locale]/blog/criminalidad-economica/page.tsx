9c4f9f61abafe00530644d1b9610db7bdfd30ae6
mcolina777-cpu <mcolina777@gmail.com>
2026-07-27 16:29:50 -0400

    Mejora de lectura en criminalidad-economica con .layout-reading

--- src/app/[locale]/blog/criminalidad-economica/page.tsx
+++ src/app/[locale]/blog/criminalidad-economica/page.tsx
@@ -29,7 +29,7 @@ export default async function BlogCriminalidadEconomica({ params }: Props) {
         </div>
       </header>
 
-      <div className="container article-container">
+      <div className="container layout-reading article-container">
         <article className="article-content">
           <p className="article-meta">{dict?.articulo_crimen?.meta}</p>
 
