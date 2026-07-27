621a79558aae457e1c4ec8b8cc30df0662b4c8ce
mcolina777-cpu <mcolina777@gmail.com>
2026-07-27 17:22:32 -0400

    Añade card-excerpt y homogeniza cards de blog

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -73,21 +73,21 @@ export default async function BlogIndex({ params }: Props) {
             <article className="card">
               <span className="section-tag">{dict?.blog?.article1?.tag}</span>
               <h3>{dict?.blog?.article1?.title}</h3>
-              <p>{dict?.blog?.article1?.desc}</p>
+              <p className="card-excerpt">{dict?.blog?.article1?.desc}</p>
               <Link href={`/${locale}/blog/criminalidad-economica`} className="card-link">{dict?.blog?.readmore}</Link>
             </article>
             
             <article className="card">
               <span className="section-tag">{dict?.blog?.article2?.tag}</span>
               <h3>{dict?.blog?.article2?.title}</h3>
-              <p>{dict?.blog?.article2?.desc}</p>
+              <p className="card-excerpt">{dict?.blog?.article2?.desc}</p>
               <Link href={`/${locale}/blog/amparo-garantia-vital`} className="card-link">{dict?.blog?.readmore}</Link>
             </article>
             {/* Note: In backup, the 3rd card was El Régimen de Poderes en el CPC y el COPP */}
             <article className="card">
               <span className="section-tag">{dict?.blog?.article4?.tag}</span>
               <h3>{dict?.blog?.article4?.title}</h3>
-              <p>{dict?.blog?.article4?.desc}</p>
+              <p className="card-excerpt">{dict?.blog?.article4?.desc}</p>
               <Link href={`/${locale}/blog/regimen-poderes-cpc-copp`} className="card-link">{dict?.blog?.readmore}</Link>
             </article>
           </div>
